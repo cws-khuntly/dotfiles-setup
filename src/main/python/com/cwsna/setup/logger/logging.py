41cd783fb4b4327e6c2ddbff5e83ee12df4b3a6f
Kevin Huntly <cws-khuntly@users.noreply.github.com>
2025-09-14 06:31:50 -0400

    Update logging.py

--- src/main/python/com/cwsna/setup/logger/logging.py
+++ src/main/python/com/cwsna/setup/logger/logging.py
@@ -16,22 +16,32 @@
 #      REVISION:  ---
 #==============================================================================
 
-import os
-import logging
-import logging.config
+from os import os
+from logging import logging
+from logging import logging.config
 
-def configureLogging(logConfigFile):
-    if (len(logConfigFile) != 0):
-        if (os.path.exists(logConfigFile)) and (os.access(logConfigFile, os.R_OK)):
+from com.cwsna.setup.enum.LoggingConfiguration import LoggingConfigur
+
+class Logger:
+    def __init__(self, logConfigFile):
+        if (len(logConfigFile) != 0):
+            if (os.path.exists(logConfigFile)) and (os.access(logConfigFile, os.R_OK)):
+                try:
+                    logging.config.fileConfig(logConfigFile)
+                except Exception as e:
+                    print(str("Failed to configure logging: {0}. No logging enabled!").format(str(e)))
+                #endtry
+            else:
+                print(str("The provided configuration file either cannot be read or does not exist."))
+            #endif
+        elif (os.path.exists(LoggingConfiguration.DEFAULT_LOG_CONFIG)) and (os.access(defaultLogConfig, os.R_OK)):
             try:
-                logging.config.fileConfig(logConfigFile)
+                logging.config.fileConfig(LoggingConfiguration.DEFAULT_LOG_CONFIG)
             except Exception as e:
                 print(str("Failed to configure logging: {0}. No logging enabled!").format(str(e)))
             #endtry
         else:
             print("Unable to load logging configuration file. No logging enabled!")
         #endif
-    else:
-        print("No logging configuration file was provided. No logging enabled!")
-    #endif
-#enddef
+    #enddef
+#endclass
